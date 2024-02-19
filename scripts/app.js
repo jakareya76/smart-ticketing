7c202e8beca137da66c3af792c47a742cee47760
jakareya76 <jakareya1306@gmail.com>
2024-02-19 13:59:41 +0600

    add event listnear on every seat button and update ticket count

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,43 @@
 const seatCardList = document.querySelectorAll(".seat_card");
+const couponCode = document.getElementById("input_coupon");
 
-function createTicketCartElement(value) {
-  const ticketCartContainer = document.getElementById("ticket-cart");
+let ticketAvailable = 40;
+let seatCount = 0;
+let ticketPrice = 550;
 
-  const element = document.createElement("div");
-  element.classList.add("flex", "items-center", "justify-between", "py-4");
-
-  element.innerHTML = `
-        <h4 class="font-sans text-[#03071288] font-medium" id="seat">
-           ${value}
-        </h4>
-        <h4 class="ml-16 font-sans text-[#03071288] font-medium">
-            Economy
-        </h4>
-        <h4 class="font-sans text-[#03071288] font-medium">
-            550tk
-        </h4>
-    `;
-
-  ticketCartContainer.appendChild(element);
-}
+document.getElementById("available-seats").innerText = ticketAvailable;
+document.getElementById("seat-selected-count").innerText = seatCount;
 
 for (const seatCard of seatCardList) {
   seatCard.addEventListener("click", function (e) {
     const seat = e.target.innerText;
 
-    createTicketCartElement(seat);
+    const seatId = e.target.id;
+
+    if (seatCount < 4) {
+      seatCount++;
+      ticketAvailable--;
+
+      createTicketCartElement(seat, seatId);
+
+      setInnerTextValue("grand-total", ticketPrice);
+      setInnerTextValue("seat-selected-count", seatCount);
+      setInnerTextValue("available-seats", ticketAvailable);
+      setInnerTextValue("total-price", ticketPrice);
+
+      ticketPrice = ticketPrice + 550;
+
+      AddBackgroundById(seatId);
+    } else {
+      alert("You Can Only Select 4 seat at a time!!");
+    }
   });
+}
+
+function applyCoupon() {
+  const coupon = couponCode.value;
+
+  if (coupon === "NEW15") {
+    console.log(coupon);
+  }
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,3 +1,31 @@
+function createTicketCartElement(value, seatId) {
+  const ticketCartContainer = document.getElementById("ticket-cart");
+
+  const element = document.createElement("div");
+  element.classList.add("flex", "items-center", "justify-between", "py-4");
+
+  element.innerHTML = `
+        <h4 class="font-sans text-[#03071288] font-medium" id="${seatId}">
+           ${value}
+        </h4>
+        <h4 class="ml-16 font-sans text-[#03071288] font-medium">
+            Economy
+        </h4>
+        <h4 class="font-sans text-[#03071288] font-medium">
+            550tk
+        </h4>
+    `;
+
+  ticketCartContainer.appendChild(element);
+}
+
 function setInnerTextValue(id, value) {
   document.getElementById(id).innerText = value;
+}
+
+function AddBackgroundById(elementId) {
+  const element = document.getElementById(elementId);
+
+  element.classList.add("bg-[#1DD100]", "text-[#fff]");
+  element.classList.remove("bg-[#0307121a]", "text-[#03071273]");
 }
